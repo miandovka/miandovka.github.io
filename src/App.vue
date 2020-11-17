37ad4413213c141a1f041311dbda39f3622c14a1
Mark Iandovka <markiandovka@gmail.com>
2020-11-17 08:47:19 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,116 @@
 <template>
+
+
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Sunburst Demo</h1>
+
+    <h2 class="centered-description">
+      sunburst - first attempt
+    </h2>
+    <div class="sunburst-parent">
+      <sunburst :data="tree">
+
+        <!-- Add behaviors -->
+        <template slot-scope="{ on, actions }">
+          <highlightOnHover v-bind="{ on, actions }"/>
+          <zoomOnClick v-bind="{ on, actions }"/>
+        </template>
+
+        <!-- Add information to be displayed on top the graph -->
+        <!--      <nodeInfoDisplayer slot="top" slot-scope="{ nodes }" :current="nodes.mouseOver" :root="nodes.root"-->
+        <!--                         description="of SSD is taken by the following file/folder"/>-->
+
+        <!-- Add bottom legend -->
+        <breadcrumbTrail slot="legend" slot-scope="{ nodes, colorGetter, width }" :current="nodes.mouseOver"
+                         :root="nodes.root" :colorGetter="colorGetter" :from="nodes.clicked" :width="width"/>
+
+      </sunburst>
+    </div>
+
+    <h2 class="centered-description">
+      sunburst - second attempt
+    </h2>
+    <div class="sunburst-d2b-parent">
+      <ChartSunburst :data="tree"></ChartSunburst>
+    </div>
+
+    <ul id="tree">
+      <treeItem
+          class="item"
+          :item="tree"
+      ></treeItem>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import {
+  breadcrumbTrail,
+  highlightOnHover,
+  // nodeInfoDisplayer,
+  sunburst,
+  zoomOnClick
+} from 'vue-d3-sunburst';
+import "vue-d3-sunburst/dist/vue-d3-sunburst.css";
+import ChartSunburst from 'vue-d2b';
+
+import treeItem from './components/treeItem.vue'
 
 export default {
   name: 'App',
+  data() {
+
+    return {
+      
+      tree: {
+
+        "name": "Home",
+        "size": 300,
+        "children": [
+          {
+            "name": "Documents",
+            "size": 200,
+            "children": [
+              {"name": "file1", "size": 50},
+              {"name": "file2", "size": 100},
+              {"name": "file3", "size": 5},
+              {"name": "file4", "size": 45}
+            ]
+          },
+          {
+            "name": "Downloads",
+            "size": 80,
+            "children": [
+              {"name": "file1", "size": 20},
+              {"name": "file2", "size": 10},
+              {"name": "file3", "size": 5},
+              {"name": "file4", "size": 30},
+              {"name": "file5", "size": 15}
+            ]
+          },
+          {
+            "name": "Music",
+            "size": 20,
+            "children": [
+              {"name": "file1", "size": 12},
+              {"name": "file2", "size": 8}
+            ]
+          }
+        ]
+      }
+    }
+  },
   components: {
-    HelloWorld
+
+    treeItem,
+    breadcrumbTrail,
+    ChartSunburst,
+    highlightOnHover,
+    // nodeInfoDisplayer,
+    sunburst,
+    zoomOnClick,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/treeItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- source: https://vuejs.org/v2/examples/tree-view.html-->
+  <li>
+    <div >
+      {{ item.name }}
+
+      <input v-model="item.size" :placeholder="item.size">
+<!--      {{ item.size }}-->
+
+      <!--      TODO: make show this only if there are children.-->
+      <span @click="toggle" v-if="item.children !== undefined">[{{ isOpen ? '-' : '+' }}]</span>
+    </div>
+    <ul v-show="isOpen">
+      <tree-item
+          class="item"
+          v-for="(child, index) in item.children"
+          :key="index"
+          :item="child"
+      ></tree-item>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'treeItem',
+  props: {
+    item: Object
+  },
+  data: function () {
+    return {
+      isOpen: false
+    };
+  },
+  methods: {
+    toggle: function () {
+      this.isOpen = !this.isOpen;
+    },
+  }
+
+}
+</script>
